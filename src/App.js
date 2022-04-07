d8490d8d533252665d665a9e402afb10df5fe153
Jana Uhrich <juhrich@costco.com>
2022-04-06 19:16:40 -0700

    creating some chrome to display the components in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@costcolabs/forge-components";
+import ShopMenuCallToAction from "./components/ShopMenuCallToAction";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="primary-outline" onClick="console.log('clicked')">
+        <img src="phone-chrome.png" className="App-logo" alt="" />
+        <ShopMenuCallToAction></ShopMenuCallToAction>
+        <Button variant="primary-outline" onClick={() => console.log("Click")}>
           Forge Is Working
         </Button>
       </header>
